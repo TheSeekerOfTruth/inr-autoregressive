da2dcc1922709718a34e464e1b343ccd1ee7c194
Mohamed Amine Mnif <elmnifmohamedamine@gmail.com>
2026-05-12 16:58:28 +0200

    Setting up the working directory and tokenizer implementation

---
+++ src/tokenizer.py
@@ -0,0 +1,43 @@
+import torch
+from collections import OrderedDict
+
+class INRTokenizer:
+    def __init__(self):
+        pass
+
+    def tokenize(self, state_dict):
+        """
+        ENCODE: state_dict(MLP model) -> tokenized_layers
+        """
+        neuron_tokens = []
+
+        # Identify layers in the model
+        keys = state_dict.keys()
+        layer_indices = sorted(list(set(int(k.split('.')[1]) for k in keys if 'seq' in k)))
+
+        for idx in layer_indices:
+            w = state_dict[f'seq.{idx}.weight']
+            b = state_dict[f'seq.{idx}.bias']           
+            
+            layer_tokens = torch.cat([w, b.unsqueeze(1)], dim=1)
+            neuron_tokens.append(list(torch.unbind(layer_tokens, dim=0)))
+        
+        # This is a List containting Lists (each List represent a Layer having all its tokens/neurons as tensors)
+        return neuron_tokens
+
+    def detokenize(self, list_of_layers):
+        """
+        DECODE: tokenized_layers -> state_dict
+        """
+        state_dict = OrderedDict()
+        
+        for i, layer_tokens in enumerate(list_of_layers):
+
+            layer_as_tensor = torch.stack(layer_tokens, dim=0) 
+            w = layer_as_tensor[:, :-1]
+            b = layer_as_tensor[:, -1]
+            
+            state_dict[f'seq.{i}.weight'] = w
+            state_dict[f'seq.{i}.bias'] = b
+            
+        return state_dict
